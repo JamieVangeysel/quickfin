38af509a887257a366f0d5af28b06367b20cada6
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2023-01-18 20:17:28 +0100

    initial expenses page code

--- frontend/src/app/pages/expenses/expenses-page.component.ts
+++ frontend/src/app/pages/expenses/expenses-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core'
+import { IJournalEntry, JournalApiService } from 'src/app/api/journal-api.service'
 
 @Component({
   selector: 'qf-expenses-page',
@@ -9,5 +10,32 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
   }
 })
 export class ExpensesPageComponent {
+  private _expenses: IJournalEntry[] = []
 
+  constructor(
+    private ref: ChangeDetectorRef,
+    private journalApi: JournalApiService
+  ) { }
+
+  async ngOnInit() {
+    try {
+      const journal = await this.journalApi.getEntries(0)
+      if (journal) {
+        this._expenses = journal.entries ?? []
+      }
+      console.log(this.expenses)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      this.ref.markForCheck()
+    }
+  }
+
+  get expenses(): IJournalEntry[] {
+    return this._expenses
+  }
+
+  get culture(): string {
+    return 'nl-BE'
+  }
 }
